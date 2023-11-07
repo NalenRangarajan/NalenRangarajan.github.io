140862d6f34e538ec6b86d3e4529ff61ba67cb0c
Froggy1414 <nalenrangarajan@icloud.com>
2023-11-06 23:50:23 -0600

    check email

--- action_page.php
+++ action_page.php
@@ -1,29 +1,24 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form data
-    $sender = $_POST['email'];
+if (isset($_POST['sendEmail'])) {
     $name = $_POST['name'];
+    $email = $_POST['emailAddress'];
+    $location = $_POST['location'];
     $message = $_POST['subject'];
-    $location = $POST['location'];
 
-    // Set the email subject and sender
-    $subject = "Message from: $name";
-    $from = "sandbagclimbing1@gmail.com"; 
+    $to = 'sandbagclimbing1@gmail.com'; // Replace with the recipient's email address
+    $subject = 'New Email from Website';
+    $headers = "From: $email";
 
-    // Compose the email message
-    $headers = "From: $from\r\n";
-    $headers .= "Reply-To: $from\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $emailMessage = "Name: $name\n";
+    $emailMessage .= "Email: $email\n";
+    $emailMessage .= "Location: $location\n";
+    $emailMessage .= "Message:\n$message";
 
-    // Send the email
-    mail($sender, $subject, $message, $headers);
-
-    // Redirect back to the form
-    header("Location: index.html"); 
-} else {
-    // Handle the form submission method
-    // You can provide a message here if needed
-    echo "Form submission method not allowed.";
+    mail($to, $subject, $emailMessage, $headers);
+    
+    echo "Email sent successfully!";
 }
 ?>
+
+
+
